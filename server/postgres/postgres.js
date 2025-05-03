113d22934c71eab9cdf92ee391d3778fc2f75072
MalaikaBatool123 <110975997+MalaikaBatool123@users.noreply.github.com>
2025-05-03 19:28:08 +0500

    server db changes

--- server/postgres/postgres.js
+++ server/postgres/postgres.js
@@ -2,14 +2,20 @@ import { Sequelize } from "sequelize";
 
 import { createTaskModel } from "../model/taskSchema.js";
 import { createUserModel } from "../model/userSchema.js";
-const sequelize = new Sequelize("todo", "postgres", "coding@123", {
-  host: "localhost",
-  dialect: "postgres",
-  port: 5555,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "todo",
+  process.env.DB_USER || "postgres",
+  process.env.DB_PASS || "coding@123",
+  {
+    host:process.env.DB_HOST || "localhost",
+    dialect: "postgres",
+    port: process.env.DB_PORT || 5555,
+  }
+);
 
 let taskModel = null;
 let userModel = null;
+
 const connection = async () => {
   try {
     await sequelize.authenticate();
@@ -21,9 +27,9 @@ const connection = async () => {
     taskModel.belongsTo(userModel, { foreignKey: "user_id" });
     await sequelize.sync();
   } catch (error) {
-    console.log("error")
+    console.log("error");
     console.error("Unable to connect to the database:", error);
   }
 };
 
-export { connection , taskModel, userModel };
+export { connection, taskModel, userModel };
