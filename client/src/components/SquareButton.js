8cee1da628fbaa9b8d38fac4b4b6c7faf14a8d01
MalaikaBatool123 <110975997+MalaikaBatool123@users.noreply.github.com>
2025-05-03 19:35:36 +0500

    little task card changes - frontend

--- client/src/components/SquareButton.js
+++ client/src/components/SquareButton.js
@@ -4,7 +4,7 @@ function SquareButton({text, property, className, icon, onClick}) {
     property = property || "light";
   return (
     <div className="button">
-      <button onClick={onClick} className={`mx-1 btn btn-sm btn-outline-${property} ${className}` }>{icon} {text}</button>
+      <button onClick={onClick} className={`mx-1 mb-1 btn btn-sm btn-outline-${property} ${className}` }>{icon} {text}</button>
     </div>
   );
 }
